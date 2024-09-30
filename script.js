81de409889a04a1b9e7811af29211db641861b95
Frank Xavio <frank2006x@gmail.com>
2024-09-30 18:18:03 +0530

    popup setup

---
+++ script.js
@@ -0,0 +1,7 @@
+let login_screen=document.getElementById('popup')
+function openlogin(){
+    popup.classList.add("open-login_screen")
+}
+function closelogin(){
+    popup.classList.remove("open-login_screen")
+}
